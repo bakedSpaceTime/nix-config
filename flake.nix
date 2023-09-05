46922f707cd760aa5fa00bce2cef58079483b820
bakedSpaceTime <tushyais@gmail.com>
2023-09-04 23:51:32 -0700

    put inputs alias before braces

--- flake.nix
+++ flake.nix
@@ -36,7 +36,7 @@
   #
   # The `@` syntax here is used to alias the attribute set of the
   # inputs's parameter, making it convenient to use inside the function.
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = inputs@{ self, nixpkgs, ... }: {
     nixosConfigurations = {
       # By default, NixOS will try to refer the nixosConfiguration with
       # its hostname, so the system named `nixos-test` will use this one.
